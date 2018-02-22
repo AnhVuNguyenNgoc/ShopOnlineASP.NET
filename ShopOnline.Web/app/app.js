a5a9d8d92eab84dd00df2ec3e60188297d9c61a3
phu-PC\phu <anhvu010197@gmail.com>
2018-02-22 17:50:39 +0700

    finish 22 lesson

--- ShopOnline.Web/app/app.js
+++ ShopOnline.Web/app/app.js
@@ -1,1 +1,18 @@
-﻿
+﻿/// <reference path="../Assets/admin/libs/angular/angular.js" />
+
+(function (app) {
+    angular.module('shoponline', ['shoponline.products','shoponline.common']).config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function config($stateProvider, $urlRouterProvider) {
+
+        $stateProvider.state('home', {
+            url: "/admin",
+            templateUrl: "/app/components/home/homeView.html",
+            controller:"homeController"
+        });
+
+        $urlRouterProvider.otherwise('/admin');
+    }
+})();
---
+++ ShopOnline.Web/app/components/products/product.modules.js
@@ -0,0 +1,17 @@
+﻿/// <reference path="../Assets/admin/libs/angular/angular.js" />
+
+(function (app) {
+    angular.module('shoponline.products', ['shoponline.common']).config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function config($stateProvider, $urlRouterProvider) {
+
+        $stateProvider.state('products', {
+            url: "/products",
+            templateUrl: "/app/components/products/productListView.html",
+            controller: "productListController"
+        });
+
+    }
+})();
